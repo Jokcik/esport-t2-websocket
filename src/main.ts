7e39ea998be470ffc99cc15047ce339de903501d
danil <jokcik@yandex.ru>
2019-06-17 10:14:02 +0300

    add websocket

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cluster from 'cluster';
 
+const workers: { [id: number]:  { worker: any, env: any } } = {};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.listen(3001);
+
+  console.log('START ', process.env.PORT);
+  app.listen(process.env.PORT);
 }
 
 if (cluster.isMaster) {
   // const count = Math.min(4, numCPUs);
   const count = 4;
   for (let i = 0; i < count; ++i) {
-    cluster.fork();
+    const env = { PORT: 4000 + (i + 1) };
+    const worker = cluster.fork(env);
+    workers[worker.id] = { worker, env };
   }
 }
 
 cluster.on('exit', (worker) => {
   console.log('exit cluster: ', worker.id);
-  cluster.fork();
+
+  const env = workers[worker.id].env;
+  delete workers[worker.id];
+
+  const newWorker = cluster.fork(env);
+  workers[newWorker.id] = { worker: newWorker, env};
 });
 
 if (cluster.isWorker) {
